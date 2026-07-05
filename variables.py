3d73ffa377270b11465ba859633111ceb37b0bac
Rohan Basnet <rohan3asnet76426@gmail.com>
2026-07-05 15:26:01 +0545

    variable value changed

--- variables.py
+++ variables.py
@@ -1,2 +1,6 @@
 message='hello world!'
 print(message)
+# variables are labels that you an assign to values
+#variable references a certain value
+message='changing the varaible value'
+print(message)
