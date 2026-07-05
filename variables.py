c18cbe6727793c311e6de09063bd502dde52a6b0
Rohan Basnet <rohan3asnet76426@gmail.com>
2026-07-05 21:50:19 +0545

    some operation on string

--- variables.py
+++ variables.py
@@ -4,3 +4,20 @@ print(message)
 #variable references a certain value
 message='changing the varaible value'
 print(message)
+#string operation
+#string is series of characters
+#anythin inside Quotes ' ' or " "
+first_name='John'
+last_name='Doe'
+# to insert variable value into string, place f before " "
+# put {} around the variables you want to use inside the string
+# these strings are called f-strings
+full_name=f" {first_name}{last_name}"
+# upper(), lower(), title() are called methods
+# method is followed by a set of parentheses because
+# methods often need additional information to do their work
+#but the mentioned methods donot need any information so they are empty
+print(full_name.upper())
+print(full_name.lower())
+print(full_name.title())
+print(f"Hello!, {full_name}.")
