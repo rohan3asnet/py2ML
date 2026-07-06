96a15c590b4d48fcdbfeed75c97bca9d61a707e0
Rohan Basnet <rohan3asnet76426@gmail.com>
2026-07-06 22:35:53 +0545

    tuple

---
+++ tuple.py
@@ -0,0 +1,7 @@
+# list is for storing items that can change throughout the life of program
+# values that cannot change are refered as immutable
+# immutable list is called Tuple
+# tuple used parantheses() instead of square brackets[]
+dimensions=(10, 20, 30)
+for dimension in dimensions:
+    print(dimension)
