eedbf4dc03666580230818791cae6d18604fb2fb
Rohan Basnet <rohan3asnet76426@gmail.com>
2026-07-07 14:36:07 +0545

    if-elif

---
+++ if.py
@@ -0,0 +1,17 @@
+# if conditional_test:
+      # do something
+age= 16
+if age>=18:
+    print('you can apply for liscence')
+else:
+    print('sorry, you are not eligible')
+
+date=12
+if date<7:
+    print('you get 25% off')
+elif date < 15:
+    print('you get 15% off')
+else:
+    print(" you get no discount")
+# if-elif chain doesnot compulsarily require else at the end
+# you can use elif too
