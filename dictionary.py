75e47af7093e871b71f8753eecdf6125de5dce55
Rohan Basnet <rohan3asnet76426@gmail.com>
2026-07-08 00:11:12 +0545

    loopint through dictionary

--- dictionary.py
+++ dictionary.py
@@ -24,3 +24,30 @@ print(status)
 #deleting key-value pairs
 del book_1['book purchased']
 print(book_1)
+
+#using get() to access values
+# it is used to set a default value that will be returned
+# if the requested key doesn't exist
+ball={'size': 'small','color':'white'}
+brand_name=ball.get('brand', 'there is no brand name.')
+print(brand_name)
+
+#looping through all key value pairs
+user_0 = {
+    'username' : 'CR7',
+    'first_name' : 'Cristiano',
+    'second_name' : 'Ronaldo',
+}
+
+for key, value in user_0.items():
+    print(f"\n Key: {key}")
+    print(f"Value: {value}")
+#looping through all the keys in a dictionary
+for key in user_0.keys():
+    print(f"\n Key: {key}")
+#these two are same
+for key in user_0:
+    print(f"\n Key: {key}")
+#looping through all values
+for value in user_0.values():
+    print(f"\n Value: {value}") 
