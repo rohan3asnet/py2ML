4520d0e44b3e476cc06274d3d6df16eb2ec376c2
Rohan Basnet <rohan3asnet76426@gmail.com>
2026-07-10 17:24:04 +0545

    while loop with list

--- while.py
+++ while.py
@@ -1,43 +1,43 @@
-# # while loop runs as long as certain condition is true
-# count =1
-# while count <=5:
-#     print(count)
-#     count+=1
+# while loop runs as long as certain condition is true
+count =1
+while count <=5:
+    print(count)
+    count+=1
 
-# # letting user choose when to quit
-# prompt='\n tell me something  and i will say it back to you:'
-# prompt += '\n enter quit to end the program. '
-# message=""
-# while message != 'quit':
-#     message=input(prompt)
-#     print(message)
+# letting user choose when to quit
+prompt='\n tell me something  and i will say it back to you:'
+prompt += '\n enter quit to end the program. '
+message=""
+while message != 'quit':
+    message=input(prompt)
+    print(message)
 
-# # we use flag as a signal to program
-# # flag= true raakhera program run garney ra 
-# # flag= false hudaa program stop huney, 
-# # flag= false chai kehi condition ley huney
-# # so yeso gardaa haami ley tyo set of conditions jasley flag=false
-# # banaauxa, teslai neatly organize garera raakhna sakinxa
-# prompt='\n tell me something  and i will say it back to you:'
-# prompt += '\n enter quit to end the program. '
-# flag = True
-# while flag:
-#     message=input(prompt)
+# we use flag as a signal to program
+# flag= true raakhera program run garney ra 
+# flag= false hudaa program stop huney, 
+# flag= false chai kehi condition ley huney
+# so yeso gardaa haami ley tyo set of conditions jasley flag=false
+# banaauxa, teslai neatly organize garera raakhna sakinxa
+prompt='\n tell me something  and i will say it back to you:'
+prompt += '\n enter quit to end the program. '
+flag = True
+while flag:
+    message=input(prompt)
 
-#     if message == 'quit':
-#         flag = False
-#     else:
-#         print(message)
+    if message == 'quit':
+        flag = False
+    else:
+        print(message)
 
-# # break allow to exit while loop immediately without running any remaining code in the loop
-# prompt = "\nPlease enter the name of a city you have visited:"
-# prompt += "\n(Enter 'quit' when you are finished.) "
-# while True:
-#     city = input(prompt)
-#     if city == 'quit':
-#             break
-#     else:
-#             print(f"I'd love to go to {city.title()}!")
+# break allow to exit while loop immediately without running any remaining code in the loop
+prompt = "\nPlease enter the name of a city you have visited:"
+prompt += "\n(Enter 'quit' when you are finished.) "
+while True:
+    city = input(prompt)
+    if city == 'quit':
+            break
+    else:
+            print(f"I'd love to go to {city.title()}!")
 
 # break ley k garxata, direct loop exit haanxa 
 # tara continue ley beginning of the loop maa pathaauxa
@@ -54,3 +54,19 @@ while current_number < 10:
 # or by pressing CTRL + C
 # make sure at least one part of the program makes 
 # loop's condition false or cause it to reach the break statement
+
+#using a while loop with lists and dictionaries
+# for loop doesnot allow you to modify the list
+# but while loop allow you to collect, store and organize inputs
+unread_books=['war and peace', 'Namesake', 'Siddartha', 'Dracula', 'Metamorphosis']
+read_books=[]
+
+while unread_books:
+     current_book = unread_books.pop()
+
+     print(f"Currently reading: {current_book.title()}")
+     read_books.append(current_book)
+
+print(f"\nThe following books have been completed:")
+for read_book in read_books:
+     print(read_book.title())
