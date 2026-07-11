922e9293c5db0c824f8155114d0d232144151569
Rohan Basnet <rohan3asnet76426@gmail.com>
2026-07-11 11:27:18 +0545

    positional arguments and multiple calls

--- function.py
+++ function.py
@@ -5,7 +5,17 @@ def greet_user():# this block is called function definition
 greet_user() #this is function call 
 
 #passing information to a function
-def greet_user(nickname):
+def greet_user(nickname):# nickname is parameter
+    # parameter is a piece of information that function requires to do the job
     print(f"oe {nickname.upper()}, k gardaexas?")
 
-greet_user('fuchhey')
+greet_user('fuchhey')# fuchhey is argument
+
+#positional arguments
+# when you call a function, it must match each arguments with each parameters
+def players_nickname(player, title):
+    print(f"{player} is known as {title}")
+#multiple function calls
+players_nickname('Cristiano Ronaldo', 'GOAT')
+players_nickname('Lionel Messi', 'FIFA Princess')
+print('SUUUUIIIIIIIII')
