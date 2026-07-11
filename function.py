7acffb4a2d0dfee56197607de2ed1d9387c6b65c
Rohan Basnet <rohan3asnet76426@gmail.com>
2026-07-11 11:15:34 +0545

    passing parameter to a function

--- function.py
+++ function.py
@@ -3,3 +3,9 @@ def greet_user():# this block is called function definition
     print("oe k xa?")
 
 greet_user() #this is function call 
+
+#passing information to a function
+def greet_user(nickname):
+    print(f"oe {nickname.upper()}, k gardaexas?")
+
+greet_user('fuchhey')
