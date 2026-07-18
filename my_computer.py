cc847f40a44a0c03d6f6866b5206387a8d98c8a7
Rohan Basnet <rohan3asnet76426@gmail.com>
2026-07-18 21:53:34 +0545

    importing multiple classes from a module

--- my_computer.py
+++ my_computer.py
@@ -1,5 +1,10 @@
-# importing a single class
-from computer import Computer
+# # importing a single class
+# from computer import Computer
+
+#importing multiple classed from a module
+from computer import Computer, Laptop
 
 pc= Computer('Dell', 'Inspiron', 2023)
 print(pc.get_name())
+pc1=Laptop('Lenovo', 'ThinkPad', 2016)
+print(pc1.get_name())
