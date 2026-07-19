12f6cbc472374ed2e98a782b2937d73c56476cff
Rohan Basnet <rohan3asnet76426@gmail.com>
2026-07-19 23:43:53 +0545

    accessing file lines

--- file_handling.py
+++ file_handling.py
@@ -2,4 +2,9 @@ from pathlib import Path
 
 path=Path('pi_digits.txt')
 contents=path.read_text()
-print(contents)
+# print(contents)
+
+#acessing a file's lines
+lines=contents.splitlines()
+for line in lines:
+    print(line)
