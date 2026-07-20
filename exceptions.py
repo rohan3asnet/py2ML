8bb0e7eaa55da223bd0d6a132069224367b4c998
Rohan Basnet <rohan3asnet76426@gmail.com>
2026-07-20 22:53:25 +0545

    simple date checker

--- exceptions.py
+++ exceptions.py
@@ -11,3 +11,24 @@ except ZeroDivisionError:
 # if code in try blocks works, python skips except block
 # but if code in try block caused an error
 # it looks for the except block
+
+try:
+    print("Enter your birthdate:\n")
+
+    day = int(input("Day: "))
+    month = int(input("Month: "))
+    year = int(input("Year: "))
+
+    if not (1 <= month <= 12):
+        print("Invalid month.")
+    elif not (1 <= day <= 31):
+        print("Invalid day.")
+    elif year > 2026:
+        print("Invalid year.")
+    else:
+        print(f"Your birthday is {day}/{month}/{year}")
+
+except ValueError:
+    print("Please enter only integer values.")
+
+    
