c2a088717e1960d324c66ddbedd6ee0f1b8974b9
Rohan Basnet <rohan3asnet76426@gmail.com>
2026-07-22 23:47:06 +0545

    readin user generated data

--- remember.py
+++ remember.py
@@ -1,10 +1,15 @@
 from pathlib import Path 
 import json
 
-username=input('what is your name: ')
+# username=input('what is your name: ')
 
 path=Path('username.json')
-contents=json.dumps(username)
-path.write_text(contents)
+# contents=json.dumps(username)
+# path.write_text(contents)
 
-print(f"we will remember you next time {username}")
+# print(f"we will remember you next time {username}")
+
+contents=path.read_text()
+username=json.loads(contents)
+
+print(f"welcome back, {username}")
