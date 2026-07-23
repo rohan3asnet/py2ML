523c98e03758453b9a9b0609a950b85f989af536
Rohan Basnet <rohan3asnet76426@gmail.com>
2026-07-23 11:59:38 +0545

    refactoring

--- remember.py
+++ remember.py
@@ -1,18 +1,26 @@
 from pathlib import Path 
 import json
 
+def get_stored_name(path):
+    if path.exists():
+        contents=path.read_text()
+        username=json.loads(contents)
+        return username
+    else:
+        return None
 
 
-path=Path('username.json')
-if path.exists():
-    contents=path.read_text()
-    username=json.loads(contents)
-    print(f"welcome back, {username}")
-else:
-    username=input('what is your name: ')
-    contents=json.dumps(username)
-    path.write_text(contents)
-    print(f"we will remember you next time {username}")
+def greet_user():
+    """greet user by name"""
+    path=Path('username.json')
+    username=get_stored_name(path)
+    if username:
+        print(f'welcome back, {username}')
+    else:
+        username=input('what is your name: ')
+        contents=json.dumps(username)
+        path.write_text(contents)
+        print(f"we will remember you next time {username}")
 
-
+greet_user()
 
