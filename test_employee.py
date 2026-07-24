3dad4fee8a2b9b2a7675379797b0559fc135acb6
Rohan Basnet <rohan3asnet76426@gmail.com>
2026-07-24 23:56:51 +0545

    using fixture

--- test_employee.py
+++ test_employee.py
@@ -1,11 +1,15 @@
 from employee_class import Employee
+import pytest
 
-def test_give_default_raise():
+@pytest.fixture
+def employee_info():# this function name 
     employee=Employee("John","Doe",90000)
-    employee.give_raise()
-    assert employee.salary==95000
+    return employee
 
-def test_give_custom_raise():
-    employee=Employee("John","Doe",90000)
-    employee.give_raise(20000)
-    assert employee.salary==110000
+def test_give_default_raise(employee_info):# and this parameter has to be same
+    employee_info.give_raise()
+    assert employee_info.salary==95000
+
+def test_give_custom_raise(employee_info):# parameter should match fixture function name
+    employee_info.give_raise(20000)
+    assert employee_info.salary==110000
