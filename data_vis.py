9bd94f4bdd34b2c8c3842fe1900a8badd46f50d7
Rohan Basnet <rohan3asnet76426@gmail.com>
2026-08-02 13:32:11 +0545

    data vis and matplotlib installation

---
+++ data_vis.py
@@ -0,0 +1,7 @@
+# Data visualisation --> visual representation to explore 
+# and present patterns in datasets
+# effective data visualizaion --> makes information look nice
+#                             --> meaning becomes clear to viewers
+
+# installing matplotlib
+# python3 -m pip install matplotlib
