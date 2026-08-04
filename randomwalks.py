8530a2ce6815499ea9e9797d4c0562416080412a
Rohan Basnet <rohan3asnet76426@gmail.com>
2026-08-04 23:47:42 +0545

    plotting the random walk

--- randomwalks.py
+++ randomwalks.py
@@ -8,9 +8,9 @@ class RandomWalk:
         self.y_values=[0]
 
     def fill_walk(self):
-        while len(self.xvalues) < self.num_points:
+        while len(self.x_values) < self.num_points:
 
-            x_direction=choice([1,-1])
+            x_direction=choice([1,-1])#move right(1) or left(-1)
             x_distance=choice([0,1,2,3,4])
             x_step=x_direction*x_distance
 
@@ -21,8 +21,19 @@ class RandomWalk:
             if x_step==0 and y_step==0:
                 continue
 
-            x=self.x_value[-1]+x_step
-            y=self.y_value[-1]+y_step
+            x=self.x_values[-1]+x_step
+            y=self.y_values[-1]+y_step
 
             self.x_values.append(x)
             self.y_values.append(y)
+
+import matplotlib.pyplot as plt
+
+rw=RandomWalk()
+rw.fill_walk()
+
+plt.style.use('classic')
+fig, ax=plt.subplots()
+ax.scatter(rw.x_values,rw.y_values, s=15)
+ax.set_aspect('equal')
+plt.show()
