c8c4235e384e45fb860fcdb1141a007796c16f87
Rohan Basnet <rohan3asnet76426@gmail.com>
2026-08-06 21:59:20 +0545

    plotting the starting and ending points

--- randomwalks.py
+++ randomwalks.py
@@ -37,4 +37,8 @@ fig, ax=plt.subplots()
 point_numbers=range(rw.num_points)
 ax.scatter(rw.x_values,rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=15)
 ax.set_aspect('equal')
+
+#for fist and last points
+ax.scatter(0,0, c='green', edgecolors='none', s=100)
+ax.scatter(rw.x_values[-1],rw.y_values[-1],c='red',edgecolors='none', s=100)
 plt.show()
