0cb46cb03b96c8390d040ada3dc3b109ff4de677
Rohan Basnet <rohan3asnet76426@gmail.com>
2026-08-06 22:41:39 +0545

    altering the size to fill the screen

--- randomwalks.py
+++ randomwalks.py
@@ -33,7 +33,7 @@ rw=RandomWalk(50_000)
 rw.fill_walk()
 
 plt.style.use('classic')
-fig, ax=plt.subplots()
+fig, ax=plt.subplots(figsize=(10,6), dpi=128)
 point_numbers=range(rw.num_points)
 ax.scatter(rw.x_values,rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=1)
 ax.set_aspect('equal')
