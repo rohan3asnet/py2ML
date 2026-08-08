0b20bac9793134efd988a00c57d4065647300b55
Rohan Basnet <rohan3asnet76426@gmail.com>
2026-08-08 21:33:46 +0545

    printing the header and their positions

--- weather.py
+++ weather.py
@@ -8,4 +8,5 @@ reader=csv.reader(lines)#reader() ley tyo list lai parse garxa
 header_row=next(reader)# next() ley arko line return garxa, 
 # tara yaa ekchoti maatra call gareko xu so ekchoti maatra line return garxa jun
 # header line hunxa jun chai header_row maa gayera basxa ani teslai display garako
-print(header_row)
+for index, column_header in enumerate(header_row):
+    print(index, column_header)
