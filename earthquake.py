8503db39d6de8fece917e6a2d2c288457a348889
Rohan Basnet <rohan3asnet76426@gmail.com>
2026-08-10 23:22:39 +0545

    extracting magnitudes

--- earthquake.py
+++ earthquake.py
@@ -6,4 +6,10 @@ contents=path.read_text()
 all_eq_data=json.loads(contents)
 
 all_eq_dicts=all_eq_data['features']
-print(len(all_eq_dicts))
+# print(len(all_eq_dicts))
+magnitudes=[]
+for eq_dict in all_eq_dicts:
+    magnitude=eq_dict['properties']['magnitude']
+    magnitudes.append(magnitude)
+
+print(magnitudes[:10])
