7c47f2440a04fbe7d44b00d18fb2e4fbb499e28c
Rohan Basnet <rohan3asnet76426@gmail.com>
2026-08-10 23:35:23 +0545

    extracting location data

--- earthquake.py
+++ earthquake.py
@@ -7,9 +7,15 @@ all_eq_data=json.loads(contents)
 
 all_eq_dicts=all_eq_data['features']
 # print(len(all_eq_dicts))
-magnitudes=[]
+magnitudes, longitudes, latitudes=[],[],[]
 for eq_dict in all_eq_dicts:
     magnitude=eq_dict['properties']['magnitude']
+    longitude=eq_dict['geometry']['coordinates'][0]
+    latitude=eq_dict['geometry']['coordinates'][1]
     magnitudes.append(magnitude)
+    longitudes.append(longitude)
+    latitudes.append(latitude)
 
 print(magnitudes[:10])
+print(longitudes[:10])
+print(latitudes[:10])
