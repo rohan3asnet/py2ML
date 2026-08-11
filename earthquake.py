617c7233542504bee8bb580359788e45f77cd55a
Rohan Basnet <rohan3asnet76426@gmail.com>
2026-08-11 22:53:12 +0545

    adding hover text

--- earthquake.py
+++ earthquake.py
@@ -8,14 +8,16 @@ all_eq_data=json.loads(contents)
 
 all_eq_dicts=all_eq_data['features']
 # print(len(all_eq_dicts))
-magnitudes, longitudes, latitudes=[],[],[]
+magnitudes, longitudes, latitudes, eq_titles=[],[],[],[]
 for eq_dict in all_eq_dicts:
     magnitude=eq_dict['properties']['magnitude']
     longitude=eq_dict['geometry']['coordinates'][0]
     latitude=eq_dict['geometry']['coordinates'][1]
+    eq_title=eq_dict['properties']['epicenter']
     magnitudes.append(magnitude)
     longitudes.append(longitude)
     latitudes.append(latitude)
+    eq_titles.append(eq_title)
 
 # print(magnitudes[:10])
 # print(longitudes[:10])
@@ -28,6 +30,7 @@ fig=px.scatter_map(lat=latitudes, lon=longitudes, size=magnitudes,
                    color=magnitudes,
                    color_continuous_scale='agsunset',
                    labels={'color':'Magnitude'},
+                   hover_name=eq_titles,
                    )
 
 fig.show()
