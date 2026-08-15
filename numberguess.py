b9d413f806c1c571c4b21f05c5a0a93682755581
Rohan Basnet <rohan3asnet76426@gmail.com>
2026-08-15 21:37:24 +0545

    added attemps counter

--- numberguess.py
+++ numberguess.py
@@ -1,10 +1,10 @@
 import random
 
 comp_num=random.randrange(1,100)
-
+guess_count=0
 while True:
     user_num=int(input("Enter your guess: "))
-    
+    guess_count+=1
     if comp_num > user_num:
         print("Go Higher ")
         continue
@@ -17,4 +17,4 @@ while True:
         print("Correct")
         break
 
-
+print(f"You got it in {guess_count} attemps")
