0c3ffb074d00bc7d92166ed1e1ab9a73f51100d1
Rohan Basnet <rohan3asnet76426@gmail.com>
2026-08-15 22:45:25 +0545

    added play again feature

--- numberguess.py
+++ numberguess.py
@@ -1,29 +1,35 @@
 import random
 
-comp_num=random.randrange(1,100)
-guess_count=0
-total_count=7
-print("You have only 7 attempts")
-while True:
-    print(f"You have {total_count} attempts left.")
-    guess_count+=1
-    if total_count !=0:
-        try:
-            user_num=int(input("Enter your guess: "))
-            if comp_num > user_num:
-                print("Go Higher ")
+flag= True
+while flag:
+    comp_num=random.randrange(1,100)
+    guess_count=0
+    total_count=7
+    print("You have only 7 attempts")
+    while True:
+        print(f"You have {total_count} attempts left.")
+        guess_count+=1
+        if total_count !=0:
+            try:
+                user_num=int(input("Enter your guess: "))
+                if comp_num > user_num:
+                    print("Go Higher ")
 
-            elif comp_num < user_num:
-                print("Go Lower")
+                elif comp_num < user_num:
+                    print("Go Lower")
 
-            else:
-                print("#####Correct#####")
-                print(f"You got it in {guess_count} attemps.")
-                break
-            total_count-=1
-        except ValueError:
-            print("Enter only Valid input i.e. Positive Interger.")
-    else:
-        print("Better Luck next Time.")
-        break
+                else:
+                    print("#####Correct#####")
+                    print(f"You got it in {guess_count} attempts.")
+                    break
+                total_count-=1
+            except ValueError:
+                print("Enter only Valid input i.e. Positive Interger.")
+        else:
+            print("Better Luck next Time.")
+            break
+    user_says=input("Wanna Play Again[y/n]? ")
+    if user_says == 'n':
+        flag=False
+
 
