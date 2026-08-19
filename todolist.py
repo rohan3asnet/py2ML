62a932cff5287f6bd85ed0f1e53aa5dba5d511ea
Rohan Basnet <rohan3asnet76426@gmail.com>
2026-08-19 23:38:52 +0545

    menu added

--- todolist.py
+++ todolist.py
@@ -1,12 +1,26 @@
-tasks=[]
+print("===== TO-DO LIST =====")
+print("1. Add task\n2. View tasks\n3. Remove task \n4. Quit")
+user_choice=input("\nChoose an option: ")
 
-task=input("Enter a task: ")
-tasks.append(task)
+if user_choice == '1':
 
-print("Task added!")
+    tasks=[]
 
-print("Your tasks:")
-for index, task in enumerate(tasks):
-    
-    print(f"{index}. {task}")
+    task=input("Enter a task: ")
+    tasks.append(task)
+
+    print("Task added!")
+
+elif user_choice == '2':
+
+    print("Your tasks:")
+    for index, task in enumerate(tasks):
+        
+        print(f"{index}. {task}")
+
+elif user_choice == '3':
+    print("remove task functions loading")
+
+else:
+    print("quit function loading")
 
