1d200b42091698f9bb30dad2446e684d53bb2607
Rohan Basnet <rohan3asnet76426@gmail.com>
2026-08-20 15:34:53 +0545

    handled invalid task numbers

--- todolist.py
+++ todolist.py
@@ -3,36 +3,40 @@ tasks=[]
 while True:
     print("===== TO-DO LIST =====")
     print("1. Add task\n2. View tasks\n3. Remove task \n4. Quit")
-    user_choice=input("\nChoose an option: ")
-    
-    if user_choice == '1':
-
-        task=input("Enter a task: ")
-        tasks.append(task)
-
-        print("Task added!\n")
-
-    elif user_choice == '2':
-
-        print("Your tasks:")
-        if len(tasks)==0:
-            print("Your Tasks list is empty.\n")
-        else:
-            for index, task in enumerate(tasks, start=1):
-                
-                print(f"{index}. {task}\n")
-
-    elif user_choice == '3':
-        remove_taks=int(input('Which task do you want to remove? '))
-        del tasks[remove_taks-1]
-
-        for index, task in enumerate(tasks, start=1):
-                        
-                        print(f"{index}. {task}\n")
+    try:
+        user_choice=int(input("\nChoose an option: "))
         
+        if user_choice == 1:
 
+            task=input("Enter a task: ")
+            tasks.append(task)
 
+            print("Task added!\n")
 
-    else:
-        break
+        elif user_choice == 2:
+
+            print("Your tasks:")
+            if len(tasks)==0:
+                print("Your Tasks list is empty.\n")
+            else:
+                for index, task in enumerate(tasks, start=1):
+                    
+                    print(f"{index}. {task}\n")
+
+        elif user_choice == 3:
+            remove_taks=int(input('Which task do you want to remove? '))
+            try:
+                del tasks[remove_taks-1]
+                
+                print("Task Removed!\n")
+                
+                for index, task in enumerate(tasks, start=1):           
+                    print(f"{index}. {task}\n")
+            except IndexError:
+                print("Invalid Task number.")
+        elif user_choice == 4:
+            break
+
+    except ValueError:
+        print("Invalid choice. Please try again")    
 
