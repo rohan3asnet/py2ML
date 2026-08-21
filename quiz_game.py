0f27620eb88d3a60ab94b49d3caaa6c371fbb261
Rohan Basnet <rohan3asnet76426@gmail.com>
2026-08-21 23:58:26 +0545

    question stored in dictionary

---
+++ quiz_game.py
@@ -0,0 +1,16 @@
+questions={
+    'What is the capital of Nepal?':[
+        'A. New Delhi',
+        'B. Kathmandu',
+        'C. Pokhara',
+        'D. Chitwan'
+    ],
+    'What is the writer of War and Peace?':[
+        'A. Leo Tolstoy',
+        'B. Chekhov',
+        'C. Dostoevsky',
+        'D. Pushkin'
+    ]
+
+}
+
