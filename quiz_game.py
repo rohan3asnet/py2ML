2263a0bdaf8de11aacf692050d28abdd3c993692
Rohan Basnet <rohan3asnet76426@gmail.com>
2026-08-24 22:24:46 +0545

    question hardcoded is removed to imporve code

--- quiz_game.py
+++ quiz_game.py
@@ -5,21 +5,31 @@ questions={
         'C': 'Pokhara',
         'D': 'Chitwan'
         
-    }
+    },
+    'What is the capital of India?':{
+            'A': 'New Delhi',
+            'B': 'Kathmandu',
+            'C': 'Pokhara',
+            'D': 'Chitwan'
+            
+        }
 }
 answers={
-    'What is the capital of Nepal?':'B'
+    'What is the capital of Nepal?':'B',
+    'What is the capital of India?':'A'
+
 }
 for question,options in questions.items():
     print(f"{question}")
     for option,info in options.items():
         print(f"{option}.{info}")
 
-user_choice=input("Your answer ").upper()
+    user_choice=input("Your answer ").upper()
 
-if user_choice == answers['What is the capital of Nepal?']:
-    print("Correct")
+    ques=question
+    if user_choice == answers[ques]:
+        print("Correct")
 
-else:
-    print("Wrong")
+    else:
+        print("Wrong")
 
