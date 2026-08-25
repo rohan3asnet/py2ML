9c75e849cfcfbe594f342391979aba9cc7be5a51
Rohan Basnet <rohan3asnet76426@gmail.com>
2026-08-25 13:54:23 +0545

    refactored

--- quiz_game.py
+++ quiz_game.py
@@ -22,19 +22,27 @@ answers={
 
 score=0
 count=0
-for question,options in questions.items():
+
+def display_question(question,options):
     print(f"{question}")
     for option,info in options.items():
         print(f"{option}.{info}")
 
+def get_choice():
     user_choice=input("Your answer ").upper()
+    return user_choice
 
-    ques=question
-    if user_choice == answers[ques]:
+def check_choice(question,user_choice):
+    return user_choice == answers[question]
+
+
+for question,options in questions.items():
+    display_question(question, options)
+    user_choice=get_choice()
+    if check_choice(question,user_choice):
         print("Correct")
         score+=1
         count+=1
-
     else:
         print("Wrong")
         count+=1
@@ -42,3 +50,26 @@ for question,options in questions.items():
 print("=====Quiz Complete=====")
 percentage=(score/count) *100
 print(f"Score:{score}/{count} i.e. {percentage}%")
+    
+
+
+# for question,options in questions.items():
+#     print(f"{question}")
+#     for option,info in options.items():
+#         print(f"{option}.{info}")
+
+#     user_choice=input("Your answer ").upper()
+
+#     ques=question
+#     if user_choice == answers[ques]:
+#         print("Correct")
+#         score+=1
+#         count+=1
+
+#     else:
+#         print("Wrong")
+#         count+=1
+
+# print("=====Quiz Complete=====")
+# percentage=(score/count) *100
+# print(f"Score:{score}/{count} i.e. {percentage}%")
